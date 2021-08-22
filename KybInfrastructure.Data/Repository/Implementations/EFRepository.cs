3acbcaf4e342c31d2c3fa3ab136be231169a5edd
onurkybsi <onurbpm@outlook.com>
2021-08-23 01:34:24 +0300

    IRepository was added

---
+++ KybInfrastructure.Data/Repository/Implementations/EFRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace KybInfrastructure.Data
+{
+    /// <summary>
+    /// Entity repository that represents SQL table
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public class EFRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class
+    {
+        protected readonly DbSet<TEntity> DbSet;
+
+        public EFRepository(DbContext context)
+        {
+            if (context is null)
+                throw new ArgumentNullException("DbContext cannot be null.");
+            DbSet = context.Set<TEntity>();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+            => DbSet.Where(filter).FirstOrDefault();
+
+        public IEnumerable<TEntity> GetList(Expression<Func<TEntity, bool>> filter)
+            => DbSet.Where(filter).ToList();
+
+        public void Add(TEntity entity)
+            => DbSet.Add(entity);
+
+        public void Update(TEntity entity)
+            => DbSet.Update(entity);
+
+        public void Remove(TEntity entity)
+            => DbSet.Remove(entity);
+    }
+}
---
+++ KybInfrastructure.Server/UnitOfWorkBuilder/UnitOfWorkBuilder.cs
@@ -0,0 +1,51 @@
+﻿using KybInfrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KybInfrastructure.Server
+{
+    /// <summary>
+    /// Contains UnitOfWork build strategies
+    /// </summary>
+    public static class UnitOfWorkBuilder
+    {
+        /// <summary>
+        /// Set UnitOfWork objects lifetime
+        /// </summary>
+        /// <typeparam name="TDatabaseContext">Database context used by UnitOfWork</typeparam>
+        /// <typeparam name="TIUnitOfWork">UnitOfWork interface</typeparam>
+        /// <typeparam name="TUnitOfWork">UnitOfWork implementation</typeparam>
+        /// <param name="services"></param>
+        /// <returns>IServiceCollection itself</returns>
+        public static IServiceCollection AddUnitOfWork<TDatabaseContext, TIUnitOfWork, TUnitOfWork>(this IServiceCollection services)
+            where TDatabaseContext : class, IDatabaseContext, new()
+            where TIUnitOfWork : class, IUnitOfWork
+            where TUnitOfWork : class, TIUnitOfWork
+        {
+            services.AddScoped<TDatabaseContext>();
+            services.AddScoped<TIUnitOfWork, TUnitOfWork>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Set Entity Framework UnitOfWork objects lifetime
+        /// </summary>
+        /// <typeparam name="TDatabaseContext">Database context used by Entity Framework UnitOfWork</typeparam>
+        /// <typeparam name="TIUnitOfWork">UnitOfWork interface</typeparam>
+        /// <typeparam name="TUnitOfWork">UnitOfWork implementation</typeparam>
+        /// <param name="services"></param>
+        /// <returns>IServiceCollection itself</returns>
+        public static IServiceCollection AddEfUnitOfWork<TDatabaseContext, TIUnitOfWork, TUnitOfWork>(this IServiceCollection services)
+            where TDatabaseContext : DbContext, IDatabaseContext, new()
+            where TIUnitOfWork : class, IUnitOfWork
+            where TUnitOfWork : class, TIUnitOfWork
+        {
+            services.AddDbContext<TDatabaseContext>();
+            services.AddScoped<TIUnitOfWork, TUnitOfWork>();
+
+            return services;
+        }
+
+    }
+}
